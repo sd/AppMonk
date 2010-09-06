ed550b2e99178e131d375854347212796cb613bc
Sebastian Delmont <sd@notso.net>
2010-09-06 11:23:43 -0400

    added licensing info

--- src/appmonk/tricks/AsyncTricks.java
+++ src/appmonk/tricks/AsyncTricks.java
@@ -1,36 +1,32 @@
 package appmonk.tricks;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+/*
+ * Copyright (C) 2009, 2010 Sebastian Delmont <sd@notso.net>
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *     
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions andlimitations under the License.
+ * 
+ */
+
 import java.io.Serializable;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.SocketException;
-import java.net.URL;
-import java.net.URLConnection;
-import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.*;
 
-import org.apache.http.util.ByteArrayBuffer;
-
 import android.content.Context;
-import android.graphics.*;
-import android.os.Environment;
 import android.os.Handler;
 import android.os.PowerManager;
 import android.os.Process;
 import android.util.*;
-import android.widget.*;
 
 public class AsyncTricks {
     public static final String TAG = "AsyncTricks";
